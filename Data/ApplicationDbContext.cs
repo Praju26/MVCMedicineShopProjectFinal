129d7393ddf2fcaaf99f28b8384a7ff3c81e8181
Prajakta <prajaktasutar2672001>
2021-09-16 14:28:35 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MVCMedicineShopProjectFinal.Data.Model;
+
+namespace MVCMedicineShopProjectFinal.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MVCMedicineShopProjectFinal.Data.Model.Medicine> Medicine { get; set; }
+    }
+}
---
+++ Data/Model/Medicine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMedicineShopProjectFinal.Data.Model
+{
+    public class Medicine
+    {
+        [Key]
+        public int MedicineID { get; set; }
+        public String Name { get; set; }
+        public int Quantity { get; set; }
+        public String Description { get; set; }
+        public String MedicineCode { get; set; }
+        public String Img { get; set; }
+        public String Use_in_case { get; set; }
+        public String Contradication { get; set; }
+        public String Price { get; set; }
+
+    }
+}
